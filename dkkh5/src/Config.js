1bd1c20035dfc4b468e3e1f9759a33f3032e540d
xiaogu-zhaozhao <1607634573@qq.com>
2022-06-28 16:01:51 +0800

    20220628

--- dkkh5/src/Config.js
+++ dkkh5/src/Config.js
@@ -1,6 +1,13 @@
 export default{
     //applinkopen
-    ApiUrl:"http://cmsdev.app-link.org/alucard263096/dkkh5/api/",
-    UploadPath:"https://alioss.app-link.org/alucard263096/dkkh5/",
-    FileUploadAPI:"http://cmsdev.app-link.org/alucard263096/dkkh5/fileupload"
+    // ApiUrl:"http://cmsdev.app-link.org/alucard263096/dkkh5/api/",
+    // UploadPath:"https://alioss.app-link.org/alucard263096/dkkh5/",
+    // FileUploadAPI:"http://cmsdev.app-link.org/alucard263096/dkkh5/fileupload"
+
+
+    ApiUrl:"http://cmsdev.app-link.org/alucard263096/purchase/api/",
+    UploadPath:"https://alioss.app-link.org/alucard263096/purchase/",
+    FileUploadAPI:"http://cmsdev.app-link.org/alucard263096/purchase/fileupload"
+
+    // purchase
 }
